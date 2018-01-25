1b47a0777ab0db6ae2421d5346dd3aab92b93076
Maxim Kuzmenko <lifeofcows@gmail.com>
2018-01-25 11:29:30 -0500

    Started CardScript creation

---
+++ Quest of the Round Table/Assets/Scripts/CardScripts/Adventure.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adventure : Card {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	int getBattlePoints() {
+		return 0;
+	}
+
+	int getBidPoints() {
+		return 0;
+	}
+}
