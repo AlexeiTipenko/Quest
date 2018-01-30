40d06afdb6f265a2be7ff0f5596af87b1593d8a5
Kirk Yuan <yuanjt810@gmail.com>
2018-01-30 00:11:51 -0500

    Updated board game png

---
+++ Quest of the Round Table/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+	public float cameraWidth = 206f;
+	public float cameraHeight = 189f;
+
+	void Update () 
+	{
+		// Camera has fixed width and height on every screen solution
+		float x = (100f - 100f / (Screen.width / cameraWidth)) / 100f;
+		float y = (100f - 100f / (Screen.height / cameraHeight)) / 100f;
+		GetComponent<Camera>().rect = new Rect(x, y, 1, 1);
+	}
+}
