4d56583e897e7daf288b8453731953fb35783bd6
Alexei Tipenko <atipenko2@gmail.com>
2018-01-31 20:13:37 -0500

    Basic prefab positioning/movement

---
+++ Quest of the Round Table/Assets/Scripts/CardDraggable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardDraggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		Debug.Log ("Begin drag.");
+		this.transform.SetParent(this.transform.parent);
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		Debug.Log ("Dragging...");
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		Debug.Log ("End drag.");
+	}
+
+}
