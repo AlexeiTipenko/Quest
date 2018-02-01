9249ebe781945f4f2ebe2be6f3d83c16fdcea7cf
Alexei Tipenko <atipenko2@gmail.com>
2018-02-01 10:24:42 -0500

    Drag/drop functionality progress (issue with panels shifting)

--- Quest of the Round Table/Assets/Scripts/CardDraggable.cs
+++ Quest of the Round Table/Assets/Scripts/CardDraggable.cs
@@ -5,18 +5,25 @@ using UnityEngine.EventSystems;
 
 public class CardDraggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
+	public Transform parentToReturnTo = null;
+
 	public void OnBeginDrag(PointerEventData eventData) {
 		Debug.Log ("Begin drag.");
-		this.transform.SetParent(this.transform.parent);
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent(this.transform.parent.parent);
+
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
 	}
 
 	public void OnDrag(PointerEventData eventData) {
-		Debug.Log ("Dragging...");
 		this.transform.position = eventData.position;
 	}
 
 	public void OnEndDrag(PointerEventData eventData) {
 		Debug.Log ("End drag.");
+		this.transform.SetParent(parentToReturnTo);
+
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
 	}
 
 }
---
+++ Quest of the Round Table/Assets/Scripts/GameBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameBoard : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public void OnDrop(PointerEventData eventData) {
+
+		//eventData.pointerDrag.name will give you the name of the prefab dropped
+		Debug.Log ("Dropping to " + gameObject.name);
+		this.transform.position = eventData.position;
+
+		CardDraggable card = eventData.pointerDrag.GetComponent<CardDraggable>();
+
+		if (card != null) {
+			card.parentToReturnTo = this.transform;
+		}
+			
+	}
+
+	public void OnPointerEnter(PointerEventData eventData) {
+
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+
+	}
+}
