2f9504d8bb26166d5451a5eda1255582a6ef08a7
Kirk Yuan <yuanjt810@gmail.com>
2018-02-05 19:38:29 -0500

    Added button functionality, next step to take input from user

---
+++ Quest of the Round Table/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour 
+{
+	public void LocalGame(string playerSelection){
+		SceneManager.LoadScene(playerSelection);
+	}
+	public void ExitGame(){
+		Application.Quit();
+	}
+	public void MenuGame(string menu){
+		SceneManager.LoadScene (menu);
+	}
+}
