c46e8bc4a6adb87d96d05c82feaa43922baede65
Kirk Yuan <yuanjt810@gmail.com>
2018-02-08 08:47:18 -0500

    Added names and bool to player object and passed to game manager, don't know what to do with empty names

--- Quest of the Round Table/Assets/Scripts/ButtonManager.cs
+++ Quest of the Round Table/Assets/Scripts/ButtonManager.cs
@@ -12,46 +12,13 @@ public class ButtonManager : MonoBehaviour
 	public static string[] names = new string[4];
 	public static bool[] hoomans = new bool[4];
 
-	//public InputField player1Name, player2Name, player3Name, player4Name;
-	//public Dropdown player1AI, player2AI, player3AI, player4AI;
-	public List<Player> playerList = new List<Player> ();
+	public static List<Player> playerList = new List<Player> ();
 
 	public void SwitchScene(string playerSelection){
 		SceneManager.LoadScene(playerSelection);
 	}
 
 	public void SwitchSceneWithPlayerList(string playerSelection) {
-		/*if (player1Name.text != "") {
-			string playerName = player1Name.text;
-			string isAIString = player1AI.captionText.text;
-			bool isAI = (isAIString == "Computer" ? true : false);
-			Player player = new Player (playerName, isAI);
-			playerList.Add (player);
-		}
-		if (player2Name.text != "") {
-			string playerName = player2Name.text;
-			string isAIString = player2AI.captionText.text;
-			bool isAI = (isAIString == "Computer" ? true : false);
-			Player player = new Player (playerName, isAI);
-			playerList.Add (player);
-		}
-		if (player3Name.text != "") {
-			string playerName = player3Name.text;
-			string isAIString = player3AI.captionText.text;
-			bool isAI = (isAIString == "Computer" ? true : false);
-			Player player = new Player (playerName, isAI);
-			playerList.Add (player);
-		}
-		if (player4Name.text != "") {
-			string playerName = player4Name.text;
-			string isAIString = player4AI.captionText.text;
-			bool isAI = (isAIString == "Computer" ? true : false);
-			Player player = new Player (playerName, isAI);
-			playerList.Add (player);
-		}
-		foreach (Player player in playerList) {
-			print (player.toString ());
-		}*/
 		for (int i = 0; i < 4; i++) {
 			GameObject inputFieldGo = GameObject.Find("Scene Elements/Canvas/Player" + i);
 			InputField inputFieldCo = inputFieldGo.GetComponent<InputField>();
@@ -59,6 +26,8 @@ public class ButtonManager : MonoBehaviour
 			GameObject dropDownGo = GameObject.Find ("Scene Elements/Canvas/Dropdown" + i);
 			Dropdown dropDownCo = dropDownGo.GetComponent<Dropdown> ();
 			hoomans[i] = (dropDownCo.options[dropDownCo.value].text == "Computer" ? true : false);
+			Player player = new Player (names [i], hoomans [i]);
+			playerList.Add (player);
 		}
 		SceneManager.LoadScene(playerSelection);
 	}
--- Quest of the Round Table/Assets/Scripts/GameManager.cs
+++ Quest of the Round Table/Assets/Scripts/GameManager.cs
@@ -8,9 +8,7 @@ public class GameManager : MonoBehaviour {
 
 	public static GameManager instance = null;
 	public BoardManager boardManager = null;
-	//public ButtonManager buttonManager = null;
 
-	// Use this for initialization
 	void Awake () {
 
 		if (instance == null)
@@ -23,23 +21,13 @@ public class GameManager : MonoBehaviour {
 
 		print ("Passing playerList to BoardManager");
 
-		GameObject brd = GameObject.Find ("LocalGameBtnManager");
-		//BoardManager boardManager = GetComponent<BoardManager> ();
-		ButtonManager buttonManager = brd.GetComponent<ButtonManager> ();
+		BoardManager boardManager = GetComponent<BoardManager> ();
 
-		Debug.Log ("Player name should be " + ButtonManager.names[0]);
-		Debug.Log ("Player name should be " + ButtonManager.names[1]);
-		Debug.Log ("Player name should be " + ButtonManager.names[2]);
-		Debug.Log ("Player name should be " + ButtonManager.names[3]);
+		foreach (Player player in ButtonManager.playerList) {
+			print (player.toString ());
+		}
 
-		Debug.Log ("Player computer should be " + ButtonManager.hoomans[0]);
-		Debug.Log ("Player computer should be " + ButtonManager.hoomans[1]);
-		Debug.Log ("Player computer should be " + ButtonManager.hoomans[2]);
-		Debug.Log ("Player computer should be " + ButtonManager.hoomans[3]);
-
-
-
-		//boardManager.initGame (buttonManager.playerList);
+		boardManager.initGame (ButtonManager.playerList);
 	}
 	
 	// Update is called once per frame
