3bea7c10e95dcd6bae9eb1750b0192039da354e7
Maxim Kuzmenko <lifeofcows@gmail.com>
2018-02-15 09:15:22 -0500

    A few additions

--- Quest of the Round Table/Assets/Tests/Card/Story/Event/ChivalrousDeedTest.cs
+++ Quest of the Round Table/Assets/Tests/Card/Story/Event/ChivalrousDeedTest.cs
@@ -5,11 +5,14 @@ using System.Collections;
 
 public class ChivalrousDeedTest {
 
+	//Event description: Player with both lowest rank and least amount of shields, receives 3 shields.
 	[Test]
 	public void ChivalrousDeedTestSimplePasses() {
 		Assert.IsTrue (ChivalrousDeed.frequency == 1);
 		ChivalrousDeed chivalrousDeed = new ChivalrousDeed ();
 		Assert.AreEqual ("Chivalrous Deed", chivalrousDeed.getCardName ());
+
+		BoardManagerData.getPlayers ();
 
 		chivalrousDeed.startBehaviour ();
 
---
+++ Quest of the Round Table/Assets/Tests/TestScenes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class sets up scenes for various test cases (NOT a testing file)
+public class TestScenes {
+
+	public void init() {
+
+	}
+} 
