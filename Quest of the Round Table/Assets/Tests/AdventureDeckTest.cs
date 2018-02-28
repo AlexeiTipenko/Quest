8f889ca74e2a8de461f114078daf0999cf287b14
Maxim Kuzmenko <lifeofcows@gmail.com>
2018-02-28 01:08:07 -0500

    prove correct distribution of cards (wrong branch atm but merge soon anyways)

--- Quest of the Round Table/Assets/Tests/AdventureDeckTest.cs
+++ Quest of the Round Table/Assets/Tests/AdventureDeckTest.cs
@@ -2,6 +2,9 @@
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System;
 
 public class AdventureDeckTest {
 
@@ -10,4 +13,32 @@ public class AdventureDeckTest {
 		Deck deck = new AdventureDeck ();
 		Assert.IsTrue (deck.getCards ().Count == 125);
 	}
+
+	[Test]
+	public void testCorrectDistributionOfCards() {
+		Deck deck = new AdventureDeck ();
+		List<string> cardNames = new List<string>();
+
+		foreach (Card card in deck.getCards()) {
+			cardNames.Add (card.cardImageName);
+		}
+
+		List<string> unique_cardNames = new List<string>(new HashSet<string>(cardNames));
+
+		Assert.IsTrue (unique_cardNames.Count == 32); //assert that there are 32 unique adventure cards
+
+		Dictionary<string, int> cardCountMap = cardNames.GroupBy( i => i ).ToDictionary(t => t.Key, t=> t.Count()); //first value is the name, second is the count
+
+		int totalCardCount = 0;
+		foreach (string name in unique_cardNames) {
+			Type genericType = Type.GetType(name, true);
+			int frequency = (int) genericType.GetField ("frequency").GetValue (null);
+
+			Assert.IsTrue (cardCountMap[name] == frequency);
+			totalCardCount += cardCountMap[name];
+		}
+
+		Assert.IsTrue (totalCardCount == 125);
+	}
+
 }
