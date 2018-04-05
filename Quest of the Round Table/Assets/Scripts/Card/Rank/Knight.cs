57210e7e3742f8be33c7acffc29a9a38914624b1
Kirk Yuan <yuanjt810@gmail.com>
2018-04-05 14:17:03 -0400

    Removed unecessary Debug Statements

--- Quest of the Round Table/Assets/Scripts/Card/Rank/Knight.cs
+++ Quest of the Round Table/Assets/Scripts/Card/Rank/Knight.cs
@@ -7,7 +7,6 @@ public class Knight : Rank {
 	public static int frequency = 4;
 
 	public Knight() : base ("Knight", 10, 7, new ChampionKnight ()) {
-        Debug.Log("Fuck you if you're crashing here, fuck you Knight");
     }
 
 }
--- Quest of the Round Table/Assets/Scripts/Card/Rank/Squire.cs
+++ Quest of the Round Table/Assets/Scripts/Card/Rank/Squire.cs
@@ -7,7 +7,6 @@ public class Squire : Rank {
 	public static int frequency = 4;
 
 	public Squire() : base ("Squire", 5, 5, new Knight()) {
-        Debug.Log("Fuck you if you're crashing here, fuck you squire");
     }
 
 }
